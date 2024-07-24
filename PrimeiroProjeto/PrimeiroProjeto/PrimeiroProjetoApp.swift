f56a7f772fe4537307b7f443f4725e2f897b7c75
Vinicius Cleim <viniciuscleim@MacBook-Air-de-Vinicius.local>
2024-07-24 18:04:41 -0300

    Cria projeto

---
+++ PrimeiroProjeto/PrimeiroProjeto/PrimeiroProjetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiroProjetoApp.swift
+//  PrimeiroProjeto
+//
+//  Created by Vinicius Cleim on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiroProjetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
